89a86614e89ee7b50fac8588c79df4459cf6b309
shawn <shawnantonucci2018@gmail.com>
2019-07-27 00:59:52 -0700

    instruction refactor

--- cpu.py
+++ cpu.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from instruction import LDAInstruction, SEIInstruction, CLDInstruction
 from rom import ROM
 
@@ -11,11 +13,15 @@ class CPU(object):
         self.running = True
         self.pc = None
 
-        self.instruction_mapping = {
-            bytes.fromhex("78"): SEIInstruction,
-            bytes.fromhex("D8"): CLDInstruction,
-            bytes.fromhex("A9"): LDAInstruction
-        }
+        self.instruction_classes = [
+            SEIInstruction,
+            CLDInstruction,
+            LDAInstruction
+        ]
+
+        self.instruction_class_mapping = defaultdict()
+        for instruction_class in self.instruction_classes:
+            self.instruction_class_mapping[instruction_class.indentifier_byte] = instruction_class
 
         self.rom = None
 
@@ -31,13 +37,13 @@ class CPU(object):
             indentifier_byte = self.rom.get_byte(self.pc)
 
             # turn byte into instruction
-            instruction_class = self.instruction_mapping.get(
+            instruction_class = self.instruction_class_mapping.get(
                 indentifier_byte, None)
             if instruction_class is None:
                 raise Exception("Instruction not found")
 
             # We have a valid instruction class
-            instruction = instruction_class(indentifier_byte)
+            instruction = instruction_class()
             instruction.execute()
 
             self.pc += instruction.instruction_length
--- instruction.py
+++ instruction.py
@@ -2,28 +2,43 @@ from abc import ABC, abstractmethod, abstractproperty
 
 
 class Instruction(ABC):
-    def __init__(self, indentifier_byte: bytes):
-        self.indentifier_byte = indentifier_byte
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "{} - Identifier byte: {}".format(self.__class__.__name__, self.indentifier_byte)
 
     @abstractproperty
-    def instruction_length(self):
+    def indentifier_byte(self) -> bytes:
+        return None
+
+    @abstractproperty
+    def instruction_length(self) -> int:
         return 1
 
     @abstractmethod
     def execute(self):
-            print("Identifier byte: {}".format(self.indentifier_byte))
+        print(self.__str__())
+
 
 class LDAInstruction(Instruction):
+    indentifier_byte = bytes.fromhex("A9")
     instruction_length = 2
+
     def execute(self):
         super().execute()
 
+
 class SEIInstruction(Instruction):
+    indentifier_byte = bytes.fromhex("78")
     instruction_length = 1
+
     def execute(self):
         super().execute()
 
 class CLDInstruction(Instruction):
+    indentifier_byte = bytes.fromhex("D8")
     instruction_length = 1
+
     def execute(self):
         super().execute()
